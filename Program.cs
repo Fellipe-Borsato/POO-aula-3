90a93e4ca6ae5be0606eeead442dcb6d00b3cdee
Fellipe-Borsato <fellipeborsato@gmail.com>
2024-11-01 16:46:11 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace POO_aula_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var aluno1 = new Aluno("Fellipe", 34);
+            aluno1.Turma = 34;
+            Console.WriteLine("O peimeiro nome é: " + aluno1.Nome);
+            aluno1.Nome = "Laura";
+            Console.WriteLine("O segundo nome é: " + aluno1.Nome);
+            Console.WriteLine("Sua turma é: " + aluno1.Turma);
+        }
+    }
+}
---
+++ aluno.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_aula_3
+{
+    internal class Aluno
+    {
+        string nome;
+        short turma;
+        double mediaNotas;
+
+        public Aluno(string nome, short turma)
+        {
+            this.nome = nome;
+            this.turma = turma;
+        }
+
+        //public string getNome() { return nome; }
+
+        //public void setNome(string nome){ this.nome = nome; }
+
+        public string Nome
+        {
+            get {return nome; }
+            set { nome = value; }
+        }
+
+        public int Turma { get; set; }
+    }
+}
